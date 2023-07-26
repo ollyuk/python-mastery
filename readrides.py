b14239b452f1d7dbb0ec6c86db1a671d8b6516b7
oliver <cumming.oliver@gmail.com>
2023-07-26 21:54:45 +0100

    comment and run each one to see which is fastest

--- readrides.py
+++ readrides.py
@@ -1,6 +1,9 @@
+from collections import namedtuple
 import csv
+import time
 
 
+# tuple
 def read_rides_as_tuples(filename):
     '''
     Read the bus ride data as a list of tuples
@@ -19,11 +22,102 @@ def read_rides_as_tuples(filename):
     return records
 
 
+def read_rides_as_namedtuple(filename):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            # a name tuple
+            Row = namedtuple('Row', ['route', 'date', 'daytype', 'rides'])
+            record = Row(route=route, date=date, daytype=daytype, rides=rides)
+            print(len(record))
+        # records.append(record)
+    return records
+
+
+# A class with __slots__
+class Row_slot:
+    __slots__ = ['route', 'date', 'daytype', 'rides']
+
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+# a dictionary
+def read_rides_as_dict(filename):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = {
+                'route': route,
+                'date': date,
+                'daytype': daytype,
+                'rides': rides
+            }
+            records.append(record)
+    return records
+
+
+# A class
+class Row:
+    def __init__(self, route, date, daytype, rides):
+        self.route = route
+        self.date = date
+        self.daytype = daytype
+        self.rides = rides
+
+
+def read_rides_as_class(filename, row_class):
+    '''
+    Read the bus ride data as a list of dictionaries
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headings = next(rows)  # Skip headers
+        for row in rows:
+            route = row[0]
+            date = row[1]
+            daytype = row[2]
+            rides = int(row[3])
+            record = row_class(route, date, daytype, rides)
+            records.append(record)
+    return records
+
+
 if __name__ == '__main__':
     import tracemalloc
 
     tracemalloc.start()
-    rows = read_rides_as_tuples('Data/ctabus.csv')
+    start_time = time.time()
+    # rows = read_rides_as_tuples('Data/ctabus.csv')
+    # rows = read_rides_as_namedtuple('Data/ctabus.csv')
+    # rows = read_rides_as_dict('Data/ctabus.csv')
+    # rows = read_rides_as_class('Data/ctabus.csv', Row_slot)
+    # rows = read_rides_as_class('Data/ctabus.csv', Row)
     current, peak = tracemalloc.get_traced_memory()
-    # print(f'Memory Use: Current {current }mb, Peak {peak}mb')
-    print(f'Memory Use: Current {current /(1024*1024)}MB, Peak {peak / (1024*1024)}MB')
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f'Memory Use: Current {current / (1024 * 1024):.2f}MB, Peak {peak / (1024 * 1024):.2f}MB')
+    print(f'Elapsed Time: {elapsed_time:.2f}s')
