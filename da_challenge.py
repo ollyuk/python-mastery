ca187829f342a95d1a5cbf307fff5d0eb03436c5
Oliver <cumming01@cardiff.ac.uk>
2023-08-01 21:41:45 +0100

    2_2 Total passengers on given date and route. Total passengers forever

--- da_challenge.py
+++ da_challenge.py
@@ -7,7 +7,13 @@ import readrides
 # - rides: Column 3. Total number of riders (integer)
 
 
-
 rows = readrides.read_rides_as_tuples('Data/ctabus.csv')
 # return a set
-print(f'Unique routes in Chicago {len({row for row in rows}):,}')
+print(f'Unique routes in Chicago {len({route[0] for route in rows}):,}')
+
+travel_date = '02/02/2001'
+date2_2_01 = [row for row in rows if row[1] == travel_date]
+
+print(f'Passengers on route 2, {travel_date} - {sum([pas[3] for pas in date2_2_01 if pas[0]=="2"])}')
+
+print(f'Passengers on all routes - {sum([pas[3] for pas in date2_2_01 ]):,}')
