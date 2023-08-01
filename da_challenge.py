207283038709241307cbd43368d85eadc35ab3f6
Oliver <cumming01@cardiff.ac.uk>
2023-08-01 22:15:54 +0100

    2_2 get the total passengers for 2001 and 2011 grouped by route

--- da_challenge.py
+++ da_challenge.py
@@ -1,4 +1,5 @@
 import readrides
+from collections import Counter
 
 # There are 4 columns of data.
 # - route: Column 0.  The bus route name.
@@ -16,4 +17,20 @@ date2_2_01 = [row for row in rows if row[1] == travel_date]
 
 print(f'Passengers on route 2, {travel_date} - {sum([pas[3] for pas in date2_2_01 if pas[0]=="2"])}')
 
-print(f'Passengers on all routes - {sum([pas[3] for pas in date2_2_01 ]):,}')
+print(f'Passengers on all routes - {sum([pas[3] for pas in rows ]):,}')
+
+
+# all the data for the 2001 grouped by route
+
+
+date2001=[(row[0],row[3]) for row in rows if '/2001' in row[1]]
+totals2001 = Counter()
+for route in date2001:
+    totals2001[route[0]] += int(route[1])
+print(totals2001)
+
+date2011 = [(row[0],row[3]) for row in rows if '/2011' in row[1]]
+totals2011 = Counter()
+for route in date2011:
+    totals2011[route[0]] += int(route[1])
+print(totals2011)
