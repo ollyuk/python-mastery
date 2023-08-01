866e78ee85a1f5a7eecaf73026754041d3869304
Oliver <cumming01@cardiff.ac.uk>
2023-08-01 22:48:54 +0100

    2_2 minor differnces in the totals. Possibly route 15 didn't exist in 2001 ... so its counted as growing massively. Also the passengers on route 2 2/2/11 needs checking

--- da_challenge.py
+++ da_challenge.py
@@ -12,25 +12,33 @@ rows = readrides.read_rides_as_tuples('Data/ctabus.csv')
 # return a set
 print(f'Unique routes in Chicago {len({route[0] for route in rows}):,}')
 
-travel_date = '02/02/2001'
+travel_date = '02/02/2011'
 date2_2_01 = [row for row in rows if row[1] == travel_date]
 
-print(f'Passengers on route 2, {travel_date} - {sum([pas[3] for pas in date2_2_01 if pas[0]=="2"])}')
+print(f'Passengers on route 2, {travel_date} - {sum([pas[3] for pas in date2_2_01 if pas[0] == "2"])}')
 
-print(f'Passengers on all routes - {sum([pas[3] for pas in rows ]):,}')
-
+print(f'Passengers on all routes - {sum([pas[3] for pas in rows]):,}')
 
 # all the data for the 2001 grouped by route
-
-
-date2001=[(row[0],row[3]) for row in rows if '/2001' in row[1]]
+date2001 = [(row[0], row[3]) for row in rows if '/2001' in row[1]]
 totals2001 = Counter()
 for route in date2001:
     totals2001[route[0]] += int(route[1])
-print(totals2001)
+# print((totals2001))
 
-date2011 = [(row[0],row[3]) for row in rows if '/2011' in row[1]]
+date2011 = [(row[0], row[3]) for row in rows if '/2011' in row[1]]
 totals2011 = Counter()
 for route in date2011:
     totals2011[route[0]] += int(route[1])
-print(totals2011)
+# print((totals2011))
+
+# show the difference between each dict item..
+diff = {}
+for route, passengers in totals2011.items():
+    if route in totals2001:
+        diff[route] = totals2011[route] - totals2001[route]
+# print(diff)
+
+diff_c= Counter(diff)
+print('Biggest increase from 2001 - 2011')
+print(diff_c.most_common(5))
