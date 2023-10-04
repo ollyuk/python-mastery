c49765cdc8f12159cbd4cc8eb80702ee6e5c56f8
Oliver <cumming.oliver@gmail.com>
2023-10-04 21:58:28 +0100

    3_2 update square and rectangle class!

--- homework/p3_2/shapes_inheritance.py
+++ homework/p3_2/shapes_inheritance.py
@@ -6,13 +6,20 @@ class Shape:
     def area(self):
         pass
 
+    def perimeter(self):
+        pass
+
 
 class Circle(Shape):
     def area(self):
         area = self.radius * 3.14
         return area
 
-class Square(Shape):
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+    
+
+class Rectangle(Shape):
     def __init__(self, x, y):
         self.length = x
         self.height = y
@@ -21,6 +28,24 @@ class Square(Shape):
         area = self.length * self.height
         return area
 
+    def perimeter(self):
+        return (self.length + self.height) * 2
+
+
+class Square(Shape):
+    def __init__(self, x):
+        self.side = x
+
+    def area(self):
+        area = self.side**2 
+        return area
+
+    def perimeter(self):
+        return 4 * self.side
+
+
 circle = Circle(3)
 
-square = Square(3,3)
+rectangle = Rectangle(3,3)
+
+square = Square(3)
