a143576598c4f74baf0fafffe0f11d0f908bcfce
ollyuk <cumming.oliver@gmail.com>
2023-10-09 20:34:23 +0100

    ex3_3 alternate class constructor

--- stock.py
+++ stock.py
@@ -1,14 +1,22 @@
 class Stock:
+    types = (str, int, float)
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
 
     def cost(self):
         return self.shares * self.price
 
     def sell(self, nshares):
         self.shares -= nshares
+
 
 def read_portfolio(filename):
     '''
@@ -24,14 +32,16 @@ def read_portfolio(filename):
             portfolio.append(record)
     return portfolio
 
+
 def print_portfolio(portfolio):
     '''
     Make a nicely formatted table showing stock data
     '''
     print('%10s %10s %10s' % ('name', 'shares', 'price'))
-    print(('-'*10 + ' ')*3)
+    print(('-' * 10 + ' ') * 3)
     for s in portfolio:
         print('%10s %10d %10.2f' % (s.name, s.shares, s.price))
+
 
 if __name__ == '__main__':
     portfolio = read_portfolio('Data/portfolio.csv')
