cda7a7e13f1780efcfa811c76f5c467bf53e8451
oliver cumming <cumming.oliver@gmail.com>
2024-02-25 19:21:22 +0000

    ex3_3 part D do the same with the reader class. Allow user to specify the data source and the type of stock to use

--- ex3_3.py
+++ ex3_3.py
@@ -2,6 +2,7 @@
 from stock import Stock
 from stock import DStock
 import stock
+from reader import read_csv_as_instances
 
 row = ['AA', '100', '32.20']
 
@@ -16,3 +17,7 @@ print(f'{sd.name=}, {sd.price=} * {sd.shares=}={sd.cost()}')
 
 pf = stock.read_portfolio_cust('Data/portfolio.csv', DStock)
 stock.print_portfolio(pf)
+
+# create a list stocks by specifying the source and Stock class to use.
+portfolio = read_csv_as_instances('Data/portfolio.csv', DStock)
+stock.print_portfolio(portfolio)
--- reader.py
+++ reader.py
@@ -3,6 +3,21 @@ import csv
 import tracemalloc
 import collections
 
+def read_csv_as_instances(filename, cls):
+    '''
+    Read a CSV file into a list of instances
+    :param filename:
+    :param cls:
+    :return:
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            records.append(cls.from_row(row))
+    return records
+
 
 def read_csv_as_dicts(filepath, coltypes):
     f = open(filepath)
